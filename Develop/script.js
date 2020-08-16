a8bffae96ee033974a009ba8d8a0c8a286178ca2
spatial theory <68703408+Sarah-Safarzadeh@users.noreply.github.com>
2020-08-16 11:59:25 -0500

    changed alerts to confirms

--- Develop/script.js
+++ Develop/script.js
@@ -5,10 +5,10 @@
   // Password Length Prompt
 window.prompt("Please enter desired password length (must be between 8 - 128 characters).");
   // Password Character Type Alerts
-window.alert("Would you like your password to include lowercase letters? If yes, please select 'ok.' If not, please select 'cancel'");
-window.alert("How do you feel about uppercase letters? If you'd like them in your password, please select 'ok.' If not, please select 'cancel.'");
-window.alert("Do you find passwords with numbers to be more agreeable? To include numbers in your password, please select 'ok.' if not, please select 'cancel.'");
-window.alert("Would you like your password to be as special as you are? To add special characters, please select 'ok.' if not, please select 'cancel.'");
+window.confirm("Would you like your password to include lowercase letters? If yes, please select 'ok.' If not, please select 'cancel'");
+window.confirm("How do you feel about uppercase letters? If you'd like them in your password, please select 'ok.' If not, please select 'cancel.'");
+window.confirm("Do you find passwords with numbers to be more agreeable? To include numbers in your password, please select 'ok.' if not, please select 'cancel.'");
+window.confirm("Would you like your password to be as special as you are? To add special characters, please select 'ok.' if not, please select 'cancel.'");
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
