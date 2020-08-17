3aabb65e3e90723dde4ed2f53075880a48f092e1
spatial theory <68703408+Sarah-Safarzadeh@users.noreply.github.com>
2020-08-16 22:16:28 -0500

    added links to readme.md

--- Develop/script.js
+++ Develop/script.js
@@ -9,33 +9,35 @@ var ABC = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var num = "0123456789";
 var special = "!#$%&*+-=?@^_~"
 
+window.alert("Welcome to Password Generator! Click that red button to get started!");
+
 var generatePassword = function () {
 
   // Password Length Prompt
   pwLength = parseInt(window.prompt("Please enter desired password length (must be between 8 - 128 characters)"));
-  while (pwLength == "" || pwLength < 8 || pwLength > 128) {
+  while (pwLength < 8 || pwLength > 128) {
     alert("Please follow the instructions. You got this!");
-    pwLength();
+    pwLength = parseInt(window.prompt("Please enter desired password length (must be between 8 - 128 characters)"));
   };
 
-  // Password Character Type Alerts
-  window.confirm("Would you like your password to include lowercase letters? If yes, please select 'ok.' If not, please select 'cancel'");
+  // Password Character Type Confirms
+  window.confirm("Would you like your password to include lowercase letters? If yes, please select 'ok.' If not, please select 'cancel.'");
   window.confirm("How do you feel about uppercase letters? If you'd like them in your password, please select 'ok.' If not, please select 'cancel.'");
-  window.confirm("Do you find passwords with numbers to be more agreeable? To include numbers in your password, please select 'ok.' if not, please select 'cancel.'");
-  window.confirm("Would you like your password to be as special as you are? To add special characters, please select 'ok.' if not, please select 'cancel.'");
+  window.confirm("Do you find passwords with numbers to be more agreeable? To include numbers in your password, please select 'ok.' If not, please select 'cancel.'");
+  window.confirm("Would you like your password to be as special as you are? To add special characters, please select 'ok.' If not, please select 'cancel.'");
+}
 
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
 
-  // Get references to the #generate element
-  var generateBtn = document.querySelector("#generate");
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
 
-  // Write password to the #password input
-  function writePassword() {
-    var password = generatePassword();
-    var passwordText = document.querySelector("#password");
+  passwordText.value = password;
 
-    passwordText.value = password;
+}
 
-  }
-
-  // Add event listener to generate button
-  generateBtn.addEventListener("click", writePassword);
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
